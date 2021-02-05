af63574232066e95e898f411bb465540e08188ef
Jorge Ktch <jorgektch@gmail.com>
2021-02-05 12:48:56 -0500

    Highest divisor success

--- february-challenge-2021-division-3/HDIVISR-Highest-Divisor.cpp
+++ february-challenge-2021-division-3/HDIVISR-Highest-Divisor.cpp
@@ -4,8 +4,9 @@ using namespace std;
 int main(){
     int n; cin>>n;
     int max_div = 1;
-    for(int d=0; d<sqrt(n); d++){
-        if(n%d==0 && d>=max_div)
+    for(int i=1; i<10+1; i++){
+        if(n%i==0 && i>=max_div) max_div = i;
     }
+    cout << max_div;
     return 0;
 }
