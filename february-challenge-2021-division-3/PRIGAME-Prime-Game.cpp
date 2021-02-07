1fe0ffc03c2e1b445f0e118cd0f894176f727f99
Jorge Ktch <jorgektch@gmail.com>
2021-02-07 01:02:04 -0500

    Prigame prepared

---
+++ february-challenge-2021-division-3/PRIGAME-Prime-Game.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(int argc, char** argv){
+     int t; cin>>t;
+    for(int c=0; c<t; c++){
+        int x; cin >> x;
+        int y; cin >> y;
+        cout << "x: "<<x<<" y: "<<y<<"\n";
+    }
+    return 0;
+}
