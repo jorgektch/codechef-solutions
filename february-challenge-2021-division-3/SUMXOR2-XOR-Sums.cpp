807bc54826fa676812cb4ce9d42cee9c886343f8
Jorge Ktch <jorgektch@gmail.com>
2021-02-08 16:35:48 -0500

    Xor sum IO done

--- february-challenge-2021-division-3/SUMXOR2-XOR-Sums.cpp
+++ february-challenge-2021-division-3/SUMXOR2-XOR-Sums.cpp
@@ -1,9 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int t; cin>>t;
-    for(int c=0; c<t; c++){
-
+    int n; cin>>n;
+    vector<int> a;
+    for(int i=0; i<n; i++){
+        int ai; cin >> ai;
+        a.push_back(ai);
     }
+    int q; cin >> q;
+    for(int i=0; i<q; i++){
+        int m; cin >> m;
+        /* Query */
+    }
+
     return 0;
 }
